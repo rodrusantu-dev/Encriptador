7006fee138adb4f1fc22599ee09202f6ebf4cc56
Rodrigo Menezes <rodrusantu7@gmail.com>
2023-05-25 10:08:03 -0300

    Inclusão Sass

--- js/copiaECola.js
+++ js/copiaECola.js
@@ -15,25 +15,3 @@ btncopiaECola.addEventListener('click', () => {
 
 });
 
-/*
-const btncopiaECola = document.getElementById('copiaECola');
-const saida = document.getElementById('saida');
-const entrada = document.getElementById('entrada');
-const aviso = document.getElementById('aviso');
-
-function copiaECola() {
-  const text = saida.value;
-  
-  navigator.clipboard.writeText(text)
-    .then(() => {
-      saida.value = '';
-      entrada.value = text;
-      aviso.innerHTML = '&nbsp;';
-    })
-    .catch((error) => {
-      console.error('Erro ao copiar para a área de transferência:', error);
-    });
-}
-
-btncopiaECola.addEventListener('click', copiaECola);
-*/
